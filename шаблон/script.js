14f68c63131917b3e45d9c22caf81266d3a5bc20
Karim <severstal.2016@gmail.com>
2023-10-08 05:11:29 +0300

    отладка кода file save load для базы

---
+++ шаблон/script.js
@@ -0,0 +1,14 @@
+var loadbtn = document.getElementById("loadbtn");
+loadbtn.addEventListener("click",clickload);
+
+var savebtn = document.getElementById("savebtn");
+savebtn.addEventListener("click",clicksave);
+
+function clickload() {
+
+}
+
+function clicksave() {
+
+}
+
